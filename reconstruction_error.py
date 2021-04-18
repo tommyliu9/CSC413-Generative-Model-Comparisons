04f5b6609863d8c3e75b3f8347fdca6c410a761b
Tommy <tommyliu9@gmail.com>
2021-04-17 20:03:38 -0400

    idk if this is right

--- reconstruction_error.py
+++ reconstruction_error.py
@@ -7,15 +7,32 @@ import torch
 from torch import nn
 from torch.nn import Parameter
 import torch.nn.functional as F
+from torch.nn.modules import loss
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision import transforms
 from six.moves.urllib.request import urlretrieve
-
+from model_wgan import DCGenerator
 from urllib.error import URLError
 from urllib.error import HTTPError
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+
+
+def log_to_tensorboard(iteration, losses):
+    writer = SummaryWriter("./runs/")
+    for key in losses:
+        arr = losses[key]
+        writer.add_scalar(f'loss/{key}', arr[-1], iteration)
+    writer.close()
+
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
 
 
 def to_var(tensor, cuda=True):
@@ -35,33 +52,85 @@ def to_var(tensor, cuda=True):
 
 
 def get_zero_vector(dim):
+    """
+    """
+    return torch.zeros(1, dim).requires_grad_()
 
-    return to_var(torch.zeros(dim))
 
-
-def get_emnist_loader(emnist_type):
+def get_emnist_loader(opts):
     transform = transforms.Compose([
-        transforms.Scale(32),
+        transforms.Scale(opts.image_size),
         transforms.ToTensor(),
         transforms.Normalize((0.5), (0.5)),
     ])
-    test = datasets.EMNIST(".", split=emnist_type,
-                           train=False, download=True, transform=transform)
+    test = datasets.EMNIST(".", split=opts.X,
+                           download=True, transform=transform)
 
     test_dloader = DataLoader(
-        dataset=test, batch_size=1, shuffle=False, num_workers=0)
+        dataset=test, batch_size=opts.batch_size, shuffle=True, num_workers=0)
     return test_dloader
 
 
-def load_model(path) -> nn.Module:
+def load_generator(opts) -> nn.Module:
     """
-    Add Your
+
     """
-    NotImplementedError
+    G_path = os.path.join(opts.load, 'G.pkl')
+    print(G_path)
+    G = DCGenerator(noise_size=opts.noise_size,
+                    conv_dim=opts.g_conv_dim, spectral_norm=False)
+
+    G.load_state_dict(torch.load(
+        G_path, map_location=lambda storage, loc: storage))
+    return G
 
 
-model = load_model("")
-z = get_zero_vector(100)
-print(z)
-z_optimizer = optim.RMSprop(model.parameters(), 5e-5)
-get_emnist_loader("letter")
+def train(opts):
+    G = load_generator(opts)
+    z = get_zero_vector(opts.noise_size)
+
+    z_optimizer = optim.SGD([z], lr=1e-4)
+    test_data = get_emnist_loader(opts)
+    train_iter = iter(test_data)
+    iter_per_epoch = len(train_iter)
+
+    criteria = nn.MSELoss()
+    iteration = 0
+    epoch = 0
+    losses = {"reconstruction_loss": []}
+    for i in range(opts.iterations):
+        sample, target = train_iter.next()
+        if iteration % iter_per_epoch == 0:
+            epoch += 1
+            train_iter = iter(test_data)
+            print("epoch:", epoch)
+
+        z_optimizer.zero_grad()
+        Loss = criteria(G(z.unsqueeze(2).unsqueeze(3)), sample)
+        Loss.backward()
+        z_optimizer.step()
+        if iteration % 1000 == 0:
+            losses["reconstruction_loss"].append(Loss)
+            log_to_tensorboard(iteration, losses)
+            print('iteration', iteration, "loss", Loss)
+
+        iteration += 1
+
+
+if __name__ == "__main__":
+    args = AttrDict()
+    args_dict = {
+        'image_size': 32,
+        'g_conv_dim': 32,
+        'noise_size': 100,
+        'num_workers': 0,
+        'iterations': 120000,
+        'X': 'letters',
+        'batch_size': 16,
+        'load': "./pretrained_models/WGAN",
+        'log_step': 100,
+        'sample_every': 200,
+        'checkpoint_every': 1000,
+    }
+    args.update(args_dict)
+    train(args)
